5a3a56ba3c52dea0fd6e931428f0351e1e9ec7a2
Artem Tkachenko <astkach@admins-MacBook-Pro.local>
2022-11-01 06:47:56 +0300

    scroll-up added

--- script/halloween.js
+++ script/halloween.js
@@ -1,15 +1,21 @@
 let img_1 = document.querySelector('#halloween');
 let img_2 = document.querySelector('#halloween_2');
+let wolf = new Audio();
+wolf.src = "../audio/wolf_howl.mp3";
+console.log(wolf);
+wolf.volume = 0.2;
 
 img_1.addEventListener('click', eyes);
 
 function eyes() {
     if (img_1.style.opacity == '1') {
         img_1.style.opacity = '0';
+        wolf.play();
     }
     else if (img_1.style.opacity == '0') {
         img_1.style.opacity = '1';
     } else {
         img_1.style.opacity = '0';
+        wolf.play();
     }
 }
